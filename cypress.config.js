3d7faa2d474abe55e1998ca11091eda62e8a5a20
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2022-08-10 15:29:31 -0500

    7. Continues Integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  screenshotOnRunFailure: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  video: false,
-  screenshotOnRunFailure: false
+    }
   },
 });
